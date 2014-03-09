6a8e96988506d01acc7aa6f0fc1645dc65848ac3
Tristan <kraig.tri125@gmail.com>
2014-03-08 20:59:14 -0500

    Infix done

--- SlicingTree.py
+++ SlicingTree.py
@@ -12,35 +12,42 @@ class SlicingTree:
 		rect = Rectangle(id, width, height)
 		self._rectangles.append(rect)
 		
-	def generateInfix(self, listOperand, listOperator):
-		NbOperand = len(self._rectangles)
-		expression = ""
-		operator = ["-", "|"]
 	
+	def generateInfix(self, listOperand, listOperator):
+		listExpression = []
 		
-		for x in range (0, NbOperand -1):
-			listOperator.append(operator[x%2])
+		for x in listOperand:
+			expression = ""
+			expressionInvertOp = ""
+			for y in range (0, len(x)):
+				expression += x[y]
+				expressionInvertOp += x[y]
+				if (len(listOperator) != y):
+					expression += listOperator[0][y]
+					expressionInvertOp += listOperator[1][y]
+			listExpression.append(expression)
+			listExpression.append(expressionInvertOp)
+				
+		return listExpression
 		
-		for x in range (0, NbOperand):
-			expression += listOperand[x]
-			if (len(listOperator) != x):
-				expression += listOperator[x]
-		print(expression)
-		return expression
+		
+	def InfixToPostFix(self, infixExpression):
+		pass
+		
 		
 	
 	def operator_perms(self, nbOperator):
 		operator = ["-", "|"]
 		listOperator = []
+		listOperatorReverse = []
 		listOperatorPerm = []
 		for x in range (0, nbOperator):
-			listOperator.append(operator[0])
+			listOperator.append(operator[x%2])
+			listOperatorReverse.append(operator[(x+1)%2])
 		listOperatorPerm.append(listOperator)
+		listOperatorPerm.append(listOperatorReverse)
 		
-		for x in range (0, 2**nbOperator)
-			
-		
-		print(listOperatorPerm)
+		return listOperatorPerm
 		
 
 	
@@ -55,7 +62,11 @@ class SlicingTree:
 		for x in self.all_perms(operand):
 			permutations.append(x)
 		
-		print(permutations)
+		listOperatorPerm = self.operator_perms(len(self._rectangles)-1)
+		
+		listInfixExpression = self.generateInfix(permutations, listOperatorPerm)
+		
+		print(listInfixExpression)
 		
 	#http://code.activestate.com/recipes/252178/	
 	def all_perms(self, elements):
@@ -69,17 +80,53 @@ class SlicingTree:
 		
 		
 objets = []
-objets.append(("A", 2, 7))
-objets.append(("B", 5, 3))
-objets.append(("C", 5, 2))
+#objets.append(("A", 2, 7))
+#objets.append(("B", 5, 3))
+#objets.append(("C", 5, 2))
+objets.append(("0", 5, 2))
+objets.append(("8", 5, 2))
+objets.append(("9", 5, 2))
+objets.append(("1", 5, 2))
+objets.append(("3", 5, 2))
+objets.append(("5", 5, 2))
+objets.append(("7", 5, 2))
+objets.append(("8", 5, 2))
+objets.append(("9", 5, 2))
+objets.append(("0", 5, 2))
+objets.append(("1", 5, 2))
+objets.append(("3", 5, 2))
+objets.append(("5", 5, 2))
+objets.append(("8", 5, 2))
+objets.append(("9", 5, 2))
+objets.append(("0", 5, 2))
+objets.append(("3", 5, 2))
+objets.append(("5", 5, 2))
+objets.append(("1", 5, 2))
+
+
+
 
 teste = SlicingTree()
 teste.addRectangle(objets[0])
 teste.addRectangle(objets[1])
 teste.addRectangle(objets[2])
+teste.addRectangle(objets[3])
+teste.addRectangle(objets[4])
+teste.addRectangle(objets[5])
+teste.addRectangle(objets[6])
+teste.addRectangle(objets[7])
+teste.addRectangle(objets[8])
+teste.addRectangle(objets[9])
+teste.addRectangle(objets[10])
+teste.addRectangle(objets[11])
+teste.addRectangle(objets[12])
+teste.addRectangle(objets[13])
+teste.addRectangle(objets[14])
+teste.addRectangle(objets[15])
+teste.addRectangle(objets[16])
+teste.addRectangle(objets[17])
+teste.addRectangle(objets[18])
 
 
 teste.SlicingPermutations()
-#teste.generateInfix()
-teste.operator_perms(2)
 		
